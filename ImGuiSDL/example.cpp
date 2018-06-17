6d12a966717ee00335edeca05cc6ca314dafb28d
Tyyppi_77 <olli-samuli.lehmus@welho.com>
2018-06-17 13:38:24 +0300

    Cleaned API into a separate header

---
+++ ImGuiSDL/example.cpp
@@ -0,0 +1,104 @@
+#include "SDL.h"
+#undef main
+
+#include "imgui.h"
+#include "imgui_sdl.h"
+
+int main()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* window = SDL_CreateWindow("SDL2 ImGui Renderer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.DisplaySize.x = 800.0f;
+	io.DisplaySize.y = 600.0f;
+	ImGui::GetStyle().WindowRounding = 0.0f;
+	ImGui::GetStyle().AntiAliasedFill = false;
+	ImGui::GetStyle().AntiAliasedLines = false;
+
+	unsigned char* pixels;
+	int width, height;
+	io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+	// TODO: At this points you've got the texture data and you need to upload that your your graphic system:
+	uint32_t rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+#endif
+
+	SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(pixels, width, height, 32, 4 * width, rmask, gmask, bmask, amask);
+	ImGuiSDL::Texture texture{ surface, SDL_CreateTextureFromSurface(renderer, surface) };
+	io.Fonts->TexID = (void*)&texture;
+
+	ImGuiSDL::Target target(800, 600, renderer);
+
+	SDL_Texture* sheet = SDL_CreateTextureFromSurface(renderer, texture.Surface);
+
+	bool run = true;
+	while (run)
+	{
+		int wheel = 0;
+
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT) run = false;
+			else if (e.type == SDL_WINDOWEVENT)
+			{
+				if (e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+				{
+					io.DisplaySize.x = static_cast<float>(e.window.data1);
+					io.DisplaySize.y = static_cast<float>(e.window.data2);
+
+					target.Resize(e.window.data1, e.window.data2);
+				}
+			}
+			else if (e.type == SDL_MOUSEWHEEL)
+			{
+				wheel = e.wheel.y;
+			}
+		}
+
+		int mouseX, mouseY;
+		const int buttons = SDL_GetMouseState(&mouseX, &mouseY);
+
+		// Setup low-level inputs (e.g. on Win32, GetKeyboardState(), or write to those fields from your Windows message loop handlers, etc.)
+		ImGuiIO& io = ImGui::GetIO();
+		io.DeltaTime = 1.0f / 60.0f;
+		io.MousePos = ImVec2(static_cast<float>(mouseX), static_cast<float>(mouseY));
+		io.MouseDown[0] = buttons & SDL_BUTTON(SDL_BUTTON_LEFT);
+		io.MouseDown[1] = buttons & SDL_BUTTON(SDL_BUTTON_RIGHT);
+		io.MouseWheel = static_cast<float>(wheel);
+
+		ImGui::NewFrame();
+
+		ImGui::ShowDemoWindow();
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+		SDL_RenderClear(renderer);
+
+		ImGui::Render();
+		ImGuiSDL::DoImGuiRender(target, ImGui::GetDrawData());
+
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_FreeSurface(texture.Surface);
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	ImGui::DestroyContext();
+
+	return 0;
+}
